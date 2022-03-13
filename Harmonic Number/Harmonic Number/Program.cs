f3e012b24c78fd4022d20eb8cbb978fc1fedbdd0
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-13 18:01:47 +0530

    Uc6 : Harmonic Number

--- Harmonic Number/Harmonic Number/Program.cs
+++ Harmonic Number/Harmonic Number/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Harmonic Number");
+
+Console.WriteLine("Enter the Harmonic you want to find\n");
+int number = Convert.ToInt32(Console.ReadLine());
+double result = 0;
+for (double i = 1; i <= number; i++)
+{
+    result += 1 / i;
+}
+
+Console.WriteLine("The Value of Harmonic " + number + " :" + result + "\n");
+Console.ReadLine();
