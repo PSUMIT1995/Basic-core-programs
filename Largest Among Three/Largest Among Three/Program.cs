3bffcb657ad6889e69500c16d7d7b5e482995025
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-13 16:58:48 +0530

    Uc4 : Largest Among Three Numbers

--- Largest Among Three/Largest Among Three/Program.cs
+++ Largest Among Three/Largest Among Three/Program.cs
@@ -1,2 +1,35 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Largest Among Three Numbers");
+
+int num1, num2, num3;
+
+Console.Write("Input the 1st number :");
+num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the  2nd number :");
+num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input the 3rd  number :");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    if (num1 > num3)
+    {
+        Console.Write("The 1st Number is the largest among three.");
+        Console.ReadLine();
+    }
+    else
+    {
+        Console.Write("The 3rd Number is the largest among three.");
+        Console.ReadLine();
+    }
+}
+else if (num2 > num3)
+{
+    Console.Write("The 2nd Number is the largest among three");
+    Console.ReadLine();
+}
+else
+{
+    Console.Write("The 3rd Number is the largest among three");
+    Console.ReadLine();
+}
