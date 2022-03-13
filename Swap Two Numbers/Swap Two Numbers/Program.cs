b99c581a0a331ee3676a34b8f57c86a40d5d995d
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-13 18:34:02 +0530

    Uc7 : Swap Two Numbers

--- Swap Two Numbers/Swap Two Numbers/Program.cs
+++ Swap Two Numbers/Swap Two Numbers/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Swap Two Numbers");
+
+int number1, number2, temporary;
+
+Console.Write("\nEnter the First Number : ");
+number1 = Convert.ToInt32
+(Console.ReadLine());
+
+Console.Write("\nEnter the Second Number : ");
+number2 = Convert.ToInt32
+(Console.ReadLine());
+
+temporary = number1;
+number1 = number2;
+number2 = temporary;
+Console.Write("\nAfter Swapping : ");
+Console.Write("\nFirst Number : " + number1);
+Console.Write("\nSecond Number : " + number2);
+Console.ReadLine();
