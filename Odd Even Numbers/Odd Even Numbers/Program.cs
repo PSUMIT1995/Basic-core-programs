e0e243a56fd0a18f6624e26896034ebf7466b904
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-13 15:10:36 +0530

    Uc1 : Odd Even Numbers

--- Odd Even Numbers/Odd Even Numbers/Program.cs
+++ Odd Even Numbers/Odd Even Numbers/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Odd Even Numbers");
+int i;
+Console.WriteLine("Enter a Number : ");
+i = int.Parse(Console.ReadLine());
+if (i % 2 == 0)
+{
+    Console.WriteLine("Entered Number is an Even Number");
+}
+else
+{
+    Console.WriteLine("Entered Number is an Odd Number");
+}
