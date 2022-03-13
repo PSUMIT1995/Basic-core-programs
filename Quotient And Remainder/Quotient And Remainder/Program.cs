476f585d375edb6557789372fc69b7df0f04f478
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-13 15:30:57 +0530

    Uc2 : Quotient And Remainder

--- Quotient And Remainder/Quotient And Remainder/Program.cs
+++ Quotient And Remainder/Quotient And Remainder/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Quotient And Remainder");
+
+int dividend, divisor;
+Console.Write("Enter dividend:");
+dividend = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter divisor:");
+divisor = Convert.ToInt32(Console.ReadLine());
+
+
+int quotient = dividend / divisor;
+int remainder = dividend % divisor;
+
+Console.WriteLine("Dividend:{0} Divisor:{1}", dividend, divisor);
+Console.WriteLine("Quotient = " + quotient);
+Console.WriteLine("Remainder = " + remainder);
