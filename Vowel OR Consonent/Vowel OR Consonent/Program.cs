821a292b99d08cecc95c4c5beb2ccd1a04551b7f
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-13 15:53:40 +0530

    Uc3 : Vowel OR Consonent

--- Vowel OR Consonent/Vowel OR Consonent/Program.cs
+++ Vowel OR Consonent/Vowel OR Consonent/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Vowel OR Consonent");
+
+char ch;
+
+Console.WriteLine("Enter any character: ");
+ch = Convert.ToChar(Console.ReadLine());
+
+if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+{
+    Console.WriteLine(ch + " is Vowel.");
+}
+else if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+{
+    Console.WriteLine(ch + " is Consonant.");
+}
+
+Console.ReadLine();
