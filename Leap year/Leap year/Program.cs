7153070ce7423b2b75c8128f827e15f6d5fd6b6a
PSUMIT1995 <psumit191.sp@gmail.com>
2022-03-18 17:33:48 +0530

    Uc8 : Leap Year

--- Leap year/Leap year/Program.cs
+++ Leap year/Leap year/Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Leap Year");
+
+Console.WriteLine("Enter Year : ");
+int Year = Convert.ToInt16(Console.ReadLine());
+if (((Year % 4 == 0) && (Year % 100 != 0)) || (Year % 400 == 0))
+{
+    Console.WriteLine("{0} is a Leap Year.", Year);
+}
+else
+{
+    Console.WriteLine("{0} is not a Leap Year.", Year);
+}
